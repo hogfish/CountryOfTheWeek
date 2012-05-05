5d3097a86afe0bcf379390628ec89995ebecb024
Trish Khoo <trishkhoo@gmail.com>
2012-05-05 22:44:48 +1000

    Basic generated rails stuff. Everyone's gotta start somewhere.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountryOfTheWeek::Application.config.secret_token = 'bc1fadbdb83ddcf20e7400663567671f1a6c4174473833366dc734f50999c80355ee12791e58d125852179f00b09384d24db629d8f4266a7b7f1c96356d136d7'
